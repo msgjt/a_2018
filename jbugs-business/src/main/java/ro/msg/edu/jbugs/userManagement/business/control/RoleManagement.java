07e7484764ab6b10ecdcab59c03b651d53af30b7
RazvanD30 <razvandumitrascu3@gmail.com>
2018-08-20 10:47:59 +0300

    Razvan: working on RoleManagement 1.0

---
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/control/RoleManagement.java
@@ -0,0 +1,4 @@
+package ro.msg.edu.jbugs.userManagement.business.control;
+
+public interface RoleManagement {
+}
---
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/dto/RoleDTO.java
@@ -0,0 +1,4 @@
+package ro.msg.edu.jbugs.userManagement.business.dto;
+
+public class RoleDTO {
+}
