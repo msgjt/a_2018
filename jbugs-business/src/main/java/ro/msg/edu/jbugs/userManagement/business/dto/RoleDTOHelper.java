ad662e91bc39cecfd320ca71f78c57e05d7dcdb9
oanacristian <oanacristian25@yahoo.com>
2018-08-20 16:18:39 +0300

    newBugsMgmt

---
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/userManagement/business/dto/RoleDTOHelper.java
@@ -0,0 +1,35 @@
+package ro.msg.edu.jbugs.userManagement.business.dto;
+
+import ro.msg.edu.jbugs.userManagement.persistence.entity.Permission;
+import ro.msg.edu.jbugs.userManagement.persistence.entity.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDTOHelper {
+
+    public static RoleDTO fromEntity(Role role) {
+        RoleDTO roleDTO = new RoleDTO();
+
+        List<PermissionDTO> permissions = new ArrayList<>();
+        role.getPermissions().forEach(p -> permissions.add(PermissionDTOHelper.fromEntity(p)));
+
+        roleDTO.setType(role.getType());
+        roleDTO.setPermissions(permissions);
+
+        return roleDTO;
+    }
+
+    public static Role toEntity(RoleDTO roleDTO) {
+        Role role = new Role();
+
+        List<Permission> permissions = new ArrayList<>();
+        roleDTO.getPermissions().forEach(p -> permissions.add(PermissionDTOHelper.toEntity(p)));
+
+        role.setType(roleDTO.getType());
+        role.setPermissions(permissions);
+
+        return role;
+    }
+
+}
---
+++ jbugs-client/src/main/java/resources/RoleResource.java
@@ -0,0 +1,38 @@
+package resources;
+
+
+import ro.msg.edu.jbugs.userManagement.business.control.RoleManagement;
+import ro.msg.edu.jbugs.userManagement.business.dto.RoleDTO;
+import ro.msg.edu.jbugs.userManagement.business.exceptions.BusinessException;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/roles")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class RoleResource {
+
+    @EJB
+    private RoleManagement roleManagement;
+
+    @GET
+    public List<RoleDTO> getRoles() { return roleManagement.getAllRoles(); }
+
+    @POST
+    public Response updateRole(RoleDTO roleDTO) {
+        try{
+            return Response.status(Response.Status.OK)
+                    .entity(roleManagement.updateRole(roleDTO))
+                    .build();
+        }
+        catch (BusinessException e){
+            return Response.status(Response.Status.NOT_MODIFIED)
+                    .build();
+        }
+    }
+
+}
