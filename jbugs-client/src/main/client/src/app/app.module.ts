bd69779b880de1c1503460b5b49f8d529c770a8c
RazvanD30 <razvandumitrascu3@gmail.com>
2018-08-21 14:22:48 +0300

    Added home page

--- jbugs-client/src/main/client/src/app/app.module.ts
+++ jbugs-client/src/main/client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './user-management/login/login.component';
 import {RouterModule, Routes} from "@angular/router";
-import { ContentComponent } from './user-management/content/content.component';
 import {AuthenticatedGuard} from "./user-management/authenticated.guard";
 import { ErrorComponent } from './error/error.component';
 import {PopupModule} from "ng2-opd-popup";
@@ -14,19 +13,20 @@ import {RoleContentComponent} from "./role-management/role-content/role-content.
 import { ProfileComponent } from './user-management/profile/profile.component';
 import {MatTableModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HomeComponent } from './common/home/home.component';
 
 const appRoutes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: '/login'
+    path: '', pathMatch: 'full', redirectTo: '/home'
+  },
+  {
+    path: 'home', component: HomeComponent
   },
   {
     path: 'register', component: SignUpComponent
   },
   {
     path: 'login', component: LoginComponent
-  },
-  {
-    path: 'content', component: ContentComponent,  canActivate: [AuthenticatedGuard]
   },
   {
     path: 'roles', component: RoleContentComponent
@@ -47,10 +47,10 @@ const appRoutes: Routes = [
     AppComponent,
     SignUpComponent,
     LoginComponent,
-    ContentComponent,
     RoleContentComponent,
     ErrorComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
