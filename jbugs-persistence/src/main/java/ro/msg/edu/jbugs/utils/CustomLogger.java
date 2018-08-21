12c42055508aff2197a628ca0579dc534404cfcb
RazvanD30 <razvandumitrascu3@gmail.com>
2018-08-21 13:53:05 +0300

    Removed indentation from logger

--- jbugs-persistence/src/main/java/ro/msg/edu/jbugs/utils/CustomLogger.java
+++ jbugs-persistence/src/main/java/ro/msg/edu/jbugs/utils/CustomLogger.java
@@ -9,40 +9,24 @@ public class CustomLogger {
 
     private static final Logger logger = LogManager.getLogger(CustomLogger.class);
 
-    private static int indentation = 0;
-
-
-
     public static void logEnter(@NotNull final Class className,
                                 @NotNull final String methodName,
                                 final String... parameters){
-        StringBuilder indent = new StringBuilder();
-        for(int i = 0; i < indentation; i++)
-            indent.append("   ");
-        System.out.println();
-        logger.log(Level.WARN,"{}[ENTER] CLASS={}, METHOD={}, PARAMETERS={}, TIME={}", indent.toString(),className,methodName,parameters,System.currentTimeMillis());
-        indentation++;
+        logger.log(Level.INFO,"[ENTER] CLASS={}, METHOD={}, PARAMETERS={}, TIME={}",className,methodName,parameters,System.currentTimeMillis());
     }
 
     public static void logExit(@NotNull final Class className,
                                @NotNull final String methodName,
                                final String result){
-        indentation--;
-        String indent = "";
-        for(int i = 0; i < indentation; i++)
-            indent += "   ";
-        logger.log(Level.WARN,"{}[EXIT] CLASS={}, METHOD={}, RESULT={}, TIME={}",indent,className,methodName,result,System.currentTimeMillis());
+
+        logger.log(Level.INFO,"[EXIT] CLASS={}, METHOD={}, RESULT={}, TIME={}",methodName,result,System.currentTimeMillis());
 
     }
 
     public static void logException(@NotNull final Class className,
                                     @NotNull final String methodName,
                                     @NotNull final String exceptionCode){
-        indentation--;
-        StringBuilder indent = new StringBuilder();
-        for(int i = 0; i < indentation; i++)
-            indent.append("   ");
-        logger.log(Level.WARN,"{}[EXCEPTION] CLASS={}, METHOD={}, EXCEPTION_CODE={}, TIME={}", indent.toString(),className,methodName,exceptionCode,System.currentTimeMillis());
+        logger.log(Level.INFO,"[EXCEPTION] CLASS={}, METHOD={}, EXCEPTION_CODE={}, TIME={}",className,methodName,exceptionCode,System.currentTimeMillis());
     }
 
 }
