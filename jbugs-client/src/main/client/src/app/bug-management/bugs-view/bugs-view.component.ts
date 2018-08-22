b91df537e084a4e71dc478ef75e7ae205a184cac
oanacristian <oanacristian25@yahoo.com>
2018-08-22 16:47:32 +0300

    View

--- jbugs-client/src/main/client/src/app/bug-management/bugs-view/bugs-view.component.ts
+++ jbugs-client/src/main/client/src/app/bug-management/bugs-view/bugs-view.component.ts
@@ -2,7 +2,11 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {Bug, BugService} from "../services/bug.service";
 import {Popup} from "ng2-opd-popup";
 
-
+@Component({
+  selector: 'app-bugs-view',
+  templateUrl: './bugs-view.component.html',
+  styleUrls: ['./bugs-view.component.css']
+})
 export class BugsViewComponent implements OnInit {
 
   columnsToDisplay = ['title', 'description','version','fixedVersion','status', 'severity','targetDate', 'assignedTo', 'createdBy'];
@@ -26,7 +30,7 @@ export class BugsViewComponent implements OnInit {
     this.popup.options = {
       header: "Bug description",
       color: "darkred", // red, blue....
-      widthPercentage: 40, // The with of the popou measured by browser width
+      widthProsentage: 40, // The with of the popou measured by browser width
       animationDuration: 1, // in seconds, 0 = no animation
       showButtons: false, // You can hide this in case you want to use custom buttons
       animation: "fadeInDown", // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
--- jbugs-client/src/main/client/src/app/user-management/profile/profile.component.ts
+++ jbugs-client/src/main/client/src/app/user-management/profile/profile.component.ts
@@ -27,14 +27,7 @@ export class ProfileComponent implements OnInit {
 
   }
 
-  logout() {
-    if (localStorage.getItem(LSKEY)) {
-      this.router.navigate(['./login']);
-      localStorage.removeItem(LSKEY);
-      localStorage.removeItem(TOKENKEY);
-      this.loggedIn = false;
-    }
-  }
+
 
   someClickHandler(info: any): void {
     this.message = 'You have clicked on the row containing: \n' + 'Username: ' + info.username + ', First name: ' + info.firstName
@@ -42,7 +35,7 @@ export class ProfileComponent implements OnInit {
     this.popup.options = {
       header: "User info",
       color: "darkred", // red, blue....
-      widthPercentage: 30, // The with of the popou measured by browser width
+      widthProsentage: 30, // The with of the popou measured by browser width
       animationDuration: 1, // in seconds, 0 = no animation
       showButtons: false, // You can hide this in case you want to use custom buttons
       animation: "fadeInDown" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
