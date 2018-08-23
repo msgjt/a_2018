b34a748ae13a9353d8da28d3182d3357b88a87ab
RazvanD30 <razvandumitrascu3@gmail.com>
2018-08-23 10:43:56 +0300

    want to pull

--- jbugs-client/src/main/java/resources/RoleResource.java
+++ jbugs-client/src/main/java/resources/RoleResource.java
@@ -19,9 +19,19 @@ public class RoleResource {
     @EJB
     private RoleManagement roleManagement;
 
+    /**
+     * GET REST method. Will return a list of all the roleDTOs. Should be guarded from unauthorized accesses.
+     *
+     * @return
+     */
     @GET
     public List<RoleDTO> getRoles() { return roleManagement.getAllRoles(); }
 
+    /**
+     *
+     * @param roleDTO
+     * @return
+     */
     @POST
     public Response updateRole(RoleDTO roleDTO) {
         try{
