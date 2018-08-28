849d816612a93970427f1bb62efbbf2f4c931de8
triteannn <tudortritean@gmail.com>
2018-08-28 14:05:31 +0300

    JBG-008: Fixed an issue where export to excel won't export the columns Assigned to and Created By.

--- jbugs-client/src/main/client/src/app/bug-management/services/excel.service.ts
+++ jbugs-client/src/main/client/src/app/bug-management/services/excel.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as XLSX from 'xlsx';
 
 @Injectable({
@@ -13,6 +13,12 @@ export class ExcelService {
   }
 
   public exportAsExcelFile(json: any[]): void {
+    for(let key in json) {
+      if(json.hasOwnProperty(key)) {
+        json[key]['assignedTo'] = json[key]['assignedTo'].username;
+        json[key]['createdBy'] = json[key]['createdBy'].username;
+      }
+    }
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
     const workbook: XLSX.WorkBook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
     XLSX.writeFile(workbook, ExcelService.toExportFileName());
