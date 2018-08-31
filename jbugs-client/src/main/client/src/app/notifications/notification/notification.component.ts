0d4ea045fcaf11386708949388b1a8e1b889df61
triteannn <tudortritean@gmail.com>
2018-08-31 11:42:58 +0300

    Fixed a bug where mark as read would not add the new notification to the old notifications list.

--- jbugs-client/src/main/client/src/app/notifications/notification/notification.component.ts
+++ jbugs-client/src/main/client/src/app/notifications/notification/notification.component.ts
@@ -96,5 +96,6 @@ export class NotificationComponent implements OnInit {
   seeNotification(notification: Notification) {
     let index = this.displayedNewNotifications.findIndex(notif => notif.id == notification.id);
     this.displayedNewNotifications.splice(index,1);
+    this.displayedAllNotifications.push(notification);
   }
 }
